8920f1a849b9e85f5e5a62ee928c01f216f77209
ikeda <c0a2401313@edu.teu.ac.jp>
2025-11-18 17:50:30 +0900

    演習課題1-1

--- pygame_system.py
+++ pygame_system.py
@@ -23,7 +23,12 @@ def main():
     enn = pg.Surface((20, 20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
     enn.set_colorkey((0, 0, 0))
-
+    #こうかとん
+    img = pg.image.load("fig/3.png")
+    img = pg.transform.flip(img,True,False)
+    img_rct = img.get_rect()
+    img_rct.center=300,200
+        
     
 
     tmr = 0
@@ -36,26 +41,21 @@ def main():
         x=tmr%3200
         kk_img = pg.image.load("fig/pg_bg.jpg")
         #kk_img = pg.transform.scale(kk_img,(800,900))
-        screen.blit(kk_img,(-x,0))
+        screen.blit(kk_img,[-x,0])
 
         kk_img2=pg.image.load("fig/pg_bg.jpg")
         kk_img2=pg.transform.flip(kk_img2,True,False)
-        screen.blit(kk_img2,(-x+1600,0))
+        screen.blit(kk_img2,[-x+1600,0])
 
         kk_img3 = pg.image.load("fig/pg_bg.jpg")
-        screen.blit(kk_img3,(3200-x,0))
+        screen.blit(kk_img3,[3200-x,0])
 
         #txt = font.render(str(tmr), True, (255, 255, 255))
         #screen.fill((50, 50, 50))
         #screen.blit(txt, [300, 200])
         #screen.blit(enn, [100, 400])
+        img_rct.move_ip((-1,0))
 
-        #こうかとん
-        img = pg.image.load("fig/3.png")
-        img = pg.transform.flip(img,True,False)
-        img_rct = img.get_rect()
-        img_rct.center=[300,200]
-        
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
             img_rct.move_ip((0,-1))
@@ -64,7 +64,8 @@ def main():
         if key_lst[pg.K_LEFT]:
             img_rct.move_ip((-1,0))
         if key_lst[pg.K_RIGHT]:
-            img_rct.move_ip((+1,0))
+            img_rct.move_ip((+2,0))
+        
         screen.blit(img,img_rct)
         
         pg.display.update()
